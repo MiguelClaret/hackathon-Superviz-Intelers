fc85529a520b5df48e4f8e7c4019ea70bd0f9a24
yasminminario <yasmin.minario@sou.inteli.edu.br>
2024-08-23 23:38:39 -0300

    correçao signup i login

--- src/config/routes.js
+++ src/config/routes.js
@@ -22,6 +22,8 @@ module.exports.routes = {
   '/': { view: 'pages/landing' },
   '/home': { view: 'pages/homepage' },
   '/aboutus': { view: 'pages/aboutus' },
+  '/faq': { view: 'pages/faq' },
+  
 
   // User auth
   'GET /login': { view: 'pages/login' },
