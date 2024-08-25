8953857c280e66fc1aeef71774188b88732ead89
otaviovasc-inteli <otavio.vasconcelos@sou.inteli.edu.br>
2024-08-25 19:08:05 -0300

    ssl true

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -4,6 +4,7 @@ module.exports = {
     default: {
       adapter: 'sails-postgresql',
       url: 'postgresql://admin:lFwXucqSNxgCB5VuX9VSeR1UweFAowBx@dpg-cr1ucrrqf0us739j3it0-a.oregon-postgres.render.com/dbsuperviz',
+      ssl: true,
     },
   },
   sockets: {
